aa21f5228704d393d999a84297f398649ea0c736
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-07 23:31:11 -0600

    Adding rounte handlers

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const productRoutes = require('./routes/handleProducts');
 
 app.use(express.json());
+
+app.use(express.json());
+app.use('/api', productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
